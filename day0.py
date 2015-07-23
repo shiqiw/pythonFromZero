5382817dda08e3d097cfa6b7ddd5f186f1bebbaf
shiqiw <shiqiw@stanford.edu>
2015-07-22 23:23:23 -0700

    Add README and sample practice

---
+++ day0.py
@@ -0,0 +1,16 @@
+# Try and err for the basics
+# such as comments and simple manipulation
+
+# Variable
+age = 6
+intro = 'I am '
+
+# Built-in len()
+length = len(intro)
+
+# Built-in str()
+# Concatenation of string
+print(intro+str(age))
+
+#  Built-in print()
+print(length)
