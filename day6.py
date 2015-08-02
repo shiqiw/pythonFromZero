1188ddb8dfd42653503e3615eab456f2bbb49fe9
shiqiw <shiqiw@stanford.edu>
2015-08-02 00:08:08 -0700

    latched simple practice

--- day6.py
+++ day6.py
@@ -5,14 +5,14 @@ class Solution:
     # @param {integer} n
     # @return {string}
     def convertToTitle(self, n):
-        # iterative solution, faster than recursive one
+        # iterative solution, fastest
         ans = ''
         c = 'A'
         while n > 0:
             res = n % 26
             n = n / 26
             if res != 0:
-                # why use chr(c) here gives error
+                # the scope of c makes using chr(c) here gives error
                 c = chr(65 + res - 1)
             else:
                 c = 'Z'
@@ -20,6 +20,14 @@ class Solution:
                 n -= 1
             ans = c + ans
         return ans
+
+        # iterative solution 2
+        # ans = ''
+        # while n > 0:
+        #     c = chr( (n-1) % 26 + 65)
+        #     ans = c + ans
+        #     n = (n-1) / 26
+        # return ans
         
         # recursive solution
         # ans = ''
---
+++ day7.py
@@ -0,0 +1,25 @@
+# leetcode anagram
+
+class Solution:
+    # @param {string} s
+    # @param {string} t
+    # @return {boolean}
+    def isAnagram(self, s, t):
+    	# faster
+        res = 0
+        for c in s:
+            res ^= ord(c) # otherwise unsupported operand type(s) for ^=: 'int' and 'unicode'
+        for c in t:
+            res ^= ord(c)
+        return (res == 0)
+
+        # if len(s) != len(t):
+        #     return False;
+        # # s = s.split() # by default the delimiter is space
+        # list(s)
+        # list(t)
+        # s = sorted(s)
+        # t = sorted(t)
+        # if s == t:
+        #     return True
+        # return False
