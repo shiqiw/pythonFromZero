67a3712edfb40ff2e5e38b31e6462a020cceb554
shiqiw <shiqiw@stanford.edu>
2016-08-04 14:29:44 -0700

    Add gitignore

--- FileParser/Parser.py
+++ FileParser/Parser.py
@@ -4,6 +4,25 @@ import os
 import re
 import sys
 
+class PokemonRecord(object):
+	def __init__(self):
+		self.count = 0
+		self.experience = 0
+		self.candy = 0
+
+	# If not take self as parameter, get Capture() takes no arguments (1 given)
+	def Capture(self):
+		self.count += 1
+		self.experience += 100
+		self.candy += 3
+		# If not return, get 'NoneType' object has no attribute 'count'
+		return self
+
+	def Transfer(self):
+		self.count -= 1
+		self.candy += 1
+		return self
+
 def GetDataFileName():
 	name = ""
 
@@ -11,8 +30,8 @@ def GetDataFileName():
 		name = str(input("==> Input the file you want to parse:"))
 	except:
 		error = sys.exc_info()[0]
-		#print(error)
 		print("==> Skip to grind the most recent file.")
+		name = GetLatestModifiedDataFileInCurrentDirectory()
 
 	return name
 
@@ -29,37 +48,53 @@ def GetLatestModifiedDataFileInCurrentDirectory():
 			if (os.path.getmtime(f) > os.path.getmtime(name)):
 				name = f
 
-	print("==> Selected file is %s size %s byte(s)." % (name, os.path.getsize(name)))
 	return name
 
-def DefineFileNameAccordingToDateTime():
-	time = datetime.datetime.now().strftime("%Y-%m-%d-%H")
+def GetResultFileName(inputFile):
+	# Define data file name starts with location info
+	# Match looks in beginning of the string
+	# Search looks through entire string
+	locationExpression = re.compile("([a-zA-Z ]*)[0-9]*.txt")
+	location = locationExpression.match(inputFile)
 
-	return time + ".csv"
+	# This is get current time
+	#time = datetime.datetime.now().strftime("%Y-%m-%d-%H")
+	# This is get data file last  modified time
+	time = datetime.datetime.fromtimestamp(os.path.getmtime(inputFile))
+	formatTime = time.strftime("%Y-%m-%d-%H")
+
+	if location:
+		return "{0}-{1}.csv".format(location.group(1), formatTime)
+	else:
+		return formatTime + ".csv"
 
 def CompileInputFile(inputFile):
 	print("==> Start processing.")
 
 	dictionary = {}
-	catchExpression = re.compile("We caught a (.*) with")
+	captureExpression = re.compile("We caught a (.*) with")
 	transferExpression = re.compile("] (.*) was transferred.")
 
 	with open(inputFile, "r") as content:
 		for line in content:
-			catch = catchExpression.search(line)
+			capture = captureExpression.search(line)
 			transfer = transferExpression.search(line)
 
-			if catch:
-				if catch.group(1) in dictionary:
-					dictionary[catch.group(1)] += 1
+			# Bound, unbound methods
+			# Instance, static and class methods
+			if capture:
+				if capture.group(1) in dictionary:
+					dictionary[capture.group(1)] = dictionary[capture.group(1)].Capture()
 				else:
-					dictionary[catch.group(1)] = 1
+					# If not instantiate, get unbound method Capture() must be called 
+					# with PokemonRecord instance as first argument (got nothing instead)
+					dictionary[capture.group(1)] = PokemonRecord().Capture()
 
 			if transfer:
 				if transfer.group(1) in dictionary:
-					dictionary[transfer.group(1)] -= 1
+					dictionary[transfer.group(1)] = dictionary[transfer.group(1)].Transfer()
 				else:
-					dictionary[transfer.group(1)] = -1
+					dictionary[transfer.group(1)] = PokemonRecord().Transfer()
 
 	return dictionary
 
@@ -67,36 +102,46 @@ def PrintOutput(content, outputFile):
 	print("==> Start writing to output.")
 
 	output = open(outputFile, "w")
-	total = 0
+	totalCount = 0
+	totalExperience = 0
 
-	output.write("{0:<15} {1:>3}\n".format("Name", "Count"))
+	output.write("{0:<15} {1:>5} {2:>5} {3:>5}\n".format("Name", "Count", "Exp", "Candy"))
 
 	for key in content:
-		total += content[key]
-		output.write("{0:<15} {1:>3}\n".format(key, str(content[key])))
+		record = content[key]
+		totalCount += record.count
+		totalExperience += record.experience
+		output.write("{0:<12} {1:>5} {2:>5} {3:>5}\n".format(key, 
+															record.count, 
+															record.experience, 
+															record.candy))
 
 	output.write("\n\nTotal kinds: %s \n" % len(content.keys()))
-	output.write("Total count: %s" % total)
+	output.write("Total count: %s \n" % totalCount)
+	output.write("Total Exp  : %s " % totalExperience)
 
 	output.close()
-	print("==> Output size %s byte(s)." % os.path.getsize(outputFile))
+	print("==> Output file %s size %s byte(s)." % (outputFile, os.path.getsize(outputFile)))
 	return
 
 def main():
-	# Get input file
+	# Get input file. 
+	# Define data file as .txt file and output file as .csv file.
 	inputFile = GetDataFileName()
 	if inputFile == "":
-		inputFile = GetLatestModifiedDataFileInCurrentDirectory()
-	if inputFile == "":
+		print("==> No meat for grinding. Exit.")
 		return
+	else:
+		print("==> Selected file is %s size %s byte(s)." % (inputFile, 
+															os.path.getsize(inputFile)))
 
-	# Based on input file name, decide parser mode
+ 	# Based on input file name, get output file name
+	outputFile = GetResultFileName(inputFile)
 
- 	# Based on parser mode, decide output file name
-	outputFile = DefineFileNameAccordingToDateTime()
-
+	# Digest input file
 	compileContent = CompileInputFile(inputFile)	
 
+	# Print to output file
 	PrintOutput(compileContent, outputFile)
 
 	print("==> Finish.")
